42404722a2ff0eb1d35f7cdc92845d53545eec01
Shawn Crain <scrain@gmail.com>
2016-09-08 20:27:26 -0500

    initial commit, grails create-app only

---
+++ grails-app/assets/javascripts/blockheads/blockheads.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /blockheads/core/blockheads.core
+//= require /blockheads/index/blockheads.index
+
+angular.module("blockheads", [
+    "blockheads.core",
+    "blockheads.index"
+]);
