c5804c7dada1aefa857ae4cad7456b3582420dac
pkota <pkota@rgare.com>
2016-09-09 23:03:00 -0500

    login page changes

--- grails-app/assets/javascripts/blockheads/agent/controllers/agentLoginController.js
+++ grails-app/assets/javascripts/blockheads/agent/controllers/agentLoginController.js
@@ -1,14 +1,20 @@
-
 angular
     .module("blockheads.agent")
     .controller("AgentLoginController", AgentLoginController);
 
 function AgentLoginController(Agent, $stateParams, $state) {
-    var vm = this;
+     var vm = this;
     vm.agent = new Agent();
+    var max = 10, offset = 0;
 
     vm.loginAgent = function() {
-        $state.go('agent.show');
+        Agent.list({max: max, offset: offset}, function(data) {
+            angular.forEach(data, function(item){
+                if (vm.agent.username.toLowerCase() == item.username.toLowerCase() && vm.agent.password.toLowerCase() == item.password.toLowerCase() ){
+                    $state.go('agent.show');
+                }
+            })
+        });
+        $state.go('agent.login');
     };
-
 }
